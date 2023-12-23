d64bf4090d0f27171a3ec28b1877f164e9df2e09
Daniel Teel <daniel.teel.2@us.af.mil>
2023-12-23 13:13:04 -0700

    asd

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function spawnFFMPEG(formats){
             '-vf', 'format=yuv420p',
             '-r', String(fps),
             '-g', String(fps*blockSeconds),
-            '-c:v', 'libx264',
+            '-c:v', 'h264_v4l2m2m',
             '-crf', String(qual),
             '-preset', 'veryfast',
             '-tune', 'zerolatency',
